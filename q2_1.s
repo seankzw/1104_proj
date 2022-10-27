fc383520960fffeba8493c56c49c6525d2983daf
ACED07 <112172062+ACED07@users.noreply.github.com>
2022-10-27 16:50:12 +0800

    Update q2_1.s

--- q2_1.s
+++ q2_1.s
@@ -19,8 +19,8 @@ main:
         ldr r1, =add_mem   // store the address 0x21027 into r1
         bl scanf
         // below print out the user input from scanf
-        ldr r1, =add_mem    // load data from storage
-        ldr r0, =output        // printf to print format
+        ldr r1, =add_mem    // store the address 0x21027 into r1
+        ldr r0, =output     // printf to print format
         bl printf
 
         mov r0, #0             // return code/graceful exit
