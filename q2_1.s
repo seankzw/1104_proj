cc969efe38bc7d5b8cc591951bbfba5f16c48334
ACED07 <112172062+ACED07@users.noreply.github.com>
2022-10-27 16:45:58 +0800

    Update q2_1.s

--- q2_1.s
+++ q2_1.s
@@ -1,3 +1,4 @@
+
 .data
 
 input: .asciz "%s"
@@ -5,34 +6,23 @@ input: .asciz "%s"
 output: .asciz "Output: %s\n"
 prompt: .asciz "Input: "
 
-storage: .space 4         // added 4 byte buffer to store input data
+.equ    add_mem, 0x21027
 
 .text
 .global main
 main:
 
-        ldr r0, add_prompt     // loading address of prompt message in r0
+        ldr r0, =prompt     // loading address of prompt message in r0
         bl printf              // calling printf to print prompt
 
-        ldr r0, add_input     // loading first parameter of scanf
-        ldr r1, add_storage    // location to write data from format
-//      str r1, [r2]
-        bl scanf     
-    // below print out the user input from scanf
-        ldr r1, add_storage    // load data from storage
-//      ldr r1, [r2]
-        ldr r0, add_output        // printf to print format
+        ldr r0, =input     // loading first parameter of scanf
+        ldr r1, =add_mem    // location to write data from format
+        bl scanf
+        // below print out the user input from scanf
+        ldr r1, =add_mem    // load data from storage
+        ldr r0, =output        // printf to print format
         bl printf
 
         mov r0, #0             // return code/graceful exit
         mov r7, #1
         swi 0
-
-add_prompt: .word prompt // address of prompt
-add_input: .word input // address of format
-add_output: .word output // address of string
-add_storage: .word storage // address of buffer
-
-// as -o print.o print.s
-// gcc -o print print.o
-// ./print; echo$?
