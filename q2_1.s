42e657232ff2cf570a525e897e07f6f318235938
ACED07 <112172062+ACED07@users.noreply.github.com>
2022-10-28 11:59:24 +0800

    Update q2_1.s

--- q2_1.s
+++ q2_1.s
@@ -1,12 +1,12 @@
 
 .data
 
-input: .asciz "%s"
+input: .asciz "%s"                // set "%s" to input
 
-output: .asciz "Output: %s\n"
-prompt: .asciz "Input: "
+output: .asciz "Output: %s\n"     // set "Output: %s\n" to output
+prompt: .asciz "Input: "          // set "Input: " to prompt
 
-.equ    add_mem, 0x21027
+.equ    add_mem, 0x21027          // set add_mem equals address 0x21027
 
 .text
 .global main
@@ -15,14 +15,14 @@ main:
         ldr r0, =prompt     // loading address of prompt message in r0
         bl printf           // calling printf to print prompt
 
-        ldr r0, =input     // loading first parameter of scanf
-        ldr r1, =add_mem   // store the address 0x21027 into r1
+        ldr r0, =input      // loading first parameter of scanf
+        ldr r1, =add_mem    // store the address 0x21027 into r1
         bl scanf
-        // below print out the user input from scanf
+
         ldr r1, =add_mem    // store the address 0x21027 into r1
         ldr r0, =output     // printf to print format
         bl printf
 
-        mov r0, #0             // return code/graceful exit
+        mov r0, #0          // return code/graceful exit
         mov r7, #1
         swi 0
