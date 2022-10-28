e544151e70b2f8168b7e9f4c6b27a3819bd7058a
ACED07 <112172062+ACED07@users.noreply.github.com>
2022-10-28 21:27:56 +0800

    Update q2_1.s

--- q2_1.s
+++ q2_1.s
@@ -1,5 +1,5 @@
 
-.data
+.data                             // data segment
 
 input: .asciz "%s"                // define input
 
@@ -8,21 +8,21 @@ prompt: .asciz "Input: "          // define prompt
 
 .equ    add_mem, 0x21027          // sets add_mem equals address 0x21027
 
-.text
-.global main
-main:
+.text                             // text segment
+.global main                      // start assembly code
+main:                             // main function
 
         ldr r0, =prompt     // loading address of prompt message in r0
         bl printf           // calling printf to print prompt
 
         ldr r0, =input      // load input into r0
-        ldr r1, =add_mem    // store the address 0x21027 into r1
+        ldr r1, =add_mem    // load address 0x21027 into r1
         bl scanf            // scanf to store input value into add_mem
 
         ldr r1, =add_mem    // load the address 0x21027 into r1
         ldr r0, =output     // load output to r0
         bl printf           // printf to print output
 
-        mov r0, #0          //
-        mov r7, #1          //
-        swi 0               // return code
+        mov r0, #0          // move #0 to r0
+        mov r7, #1          // move #1 to r1
+        swi 0               // exit; software interrupt
