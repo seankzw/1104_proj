73e154d8f06844499d365d9d97d88f3494d29e6a
ACED07 <112172062+ACED07@users.noreply.github.com>
2022-10-29 13:28:19 +0800

    Update q2_1.s

--- q2_1.s
+++ q2_1.s
@@ -1,28 +1,26 @@
+.data // data segment
+input: .asciz "%d" // define input
+output: .asciz "Output: %d\n" // define output
+prompt: .asciz "Input: " // define prompt
 
-.data                             // data segment
+.equ    add_mem, 0x21027 // sets add_mem equals address 0x21027
 
-input: .asciz "%s"                // define input
+.text // text segment
+.global main // start assembly code
+main: // main function
 
-output: .asciz "Output: %s\n"     // define output
-prompt: .asciz "Input: "          // define prompt
+        ldr r0, =prompt // loading address of prompt message in r0
+        bl printf // calling printf to print prompt
 
-.equ    add_mem, 0x21027          // sets add_mem equals address 0x21027
+        ldr r0, =input // load input into r0
+        ldr r1, =add_mem // load address 0x21027 into r1
+        bl scanf // scanf to store input value into add_mem
 
-.text                             // text segment
-.global main                      // start assembly code
-main:                             // main function
+        ldr r2, =add_mem // load the address 0x21027 into r1
+        ldr r1, [r2] // store value of r2 address into r1
+        ldr r0, =output // load output to r0
+        bl printf // printf to print output
 
-        ldr r0, =prompt     // loading address of prompt message in r0
-        bl printf           // calling printf to print prompt
-
-        ldr r0, =input      // load input into r0
-        ldr r1, =add_mem    // load address 0x21027 into r1
-        bl scanf            // scanf to store input value into add_mem
-
-        ldr r1, =add_mem    // load the address 0x21027 into r1
-        ldr r0, =output     // load output to r0
-        bl printf           // printf to print output
-
-        mov r0, #0          // move #0 to r0
-        mov r7, #1          // move #1 to r1
-        swi 0               // exit; software interrupt
+        mov r0, #0 // move #0 to r0
+        mov r7, #1 // move #1 to r1
+        swi 0 // exit; software interrupt
