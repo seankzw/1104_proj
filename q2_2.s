e559eb5d5de342fc1bb5d3ce3507855d6b74fab7
ACED07 <112172062+ACED07@users.noreply.github.com>
2022-11-03 16:58:51 +0800

    Update q2_2.s

--- q2_2.s
+++ q2_2.s
@@ -96,18 +96,18 @@ rpi_setup: // set up raspberry pins function/label
 
 ask_opt: // ask user for input
 
-	ldr	r0, =prompt	// load prompt address into r0
-	bl	printf // print prompt
+    ldr	r0, =prompt	// load prompt address into r0
+    bl	printf // print prompt
 	
-	ldr r0, =input // load input address into r0
-	ldr	r1, =num // load num address r1
-	bl	scanf // scanf input
-	ldr r0, =opt // load option string into r0
-	ldr	r1, =num // load address of num into r1
-	ldr r1, [r1] // load value of r1 address into r1
-	bl	printf // print prompt
-	ldr r1, =num // load num address into r1
-	ldr r1, [r1] // load value of r1 into r1
+    ldr r0, =input // load input address into r0
+    ldr	r1, =num // load num address r1
+    bl	scanf // scanf input
+    ldr r0, =opt // load option string into r0
+    ldr	r1, =num // load address of num into r1
+    ldr r1, [r1] // load value of r1 address into r1
+    bl printf // print prompt
+    ldr r1, =num // load num address into r1
+    ldr r1, [r1] // load value of r1 into r1
 
     cmp r1, #1 // if user input 1
     bleq set_pin17 // set pin to 17 
