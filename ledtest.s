e0c0b51b53eb689e0ec9ee3c7142ce0b37f1e4cf
x3Grim <114715737+x3Grim@users.noreply.github.com>
2022-11-10 16:39:50 +0800

    Create ledtest.s

---
+++ ledtest.s
@@ -0,0 +1,72 @@
+@COA group assignment Question 2 Part 2
+.data
+.balign 4				@alignment of the instruction of data
+prompt: .asciz "Please enter a mode:\n" @for printf calls,by GNU assembler's ".asciz";
+redpin: .int 0				@set 
+greenpin: .int 1			@
+input: .asciz "%d"
+option: .asciz "option %d \n"
+num: .int 0 @number variable
+
+.text
+.global main
+.extern printf
+.extern wiringPiSetup
+.extern delay
+.extern digitalWrite
+.extern pinMode
+
+main:
+	bl wiringPiSetup
+	ldr r0, =redpin
+	ldr r0, [r0]
+	mov r1, #1
+	bl pinMode
+	ldr r0, =greenpin
+	ldr r0, [r0]
+	mov r1, #1
+	bl pinMode
+	ldr r0, =prompt
+	bl printf
+ 	ldr r0, =input	@load input address into r0
+   	ldr r1, =num	@load num address r1
+   	bl scanf 	@scanf input
+   	ldr r0, =option	@load option string into r0
+    	ldr r1, =num	@load address of num into r1
+  	ldr r1, [r1]	@load value of r1 address into r1
+   	bl printf 	@print prompt
+   	ldr r1, =num 	@load num address into r1
+   	ldr r1, [r1]	@load value of r1 into r1
+	cmp r1, #1 	@if user input 1
+	bleq greenLed	@set green led
+	cmp r1, #2	@if user input 2
+	bleq redLed	@set red led
+	cmp r1, #3	@if user input 2
+	bleq ledOff	@set red led
+
+greenLed:
+	ldr r0, =greenpin
+	ldr r0, [r0]
+	mov r1, #1
+	bl digitalWrite
+	b main
+
+redLed:
+	ldr r0,=redpin
+	ldr r0,[r0]
+	mov r1, #1
+	bl digitalWrite
+	b main
+
+ledOff:
+	ldr r0,=greenpin
+   	ldr r0,[r0]
+	mov r1, #0
+	bl digitalWrite
+	ldr r0,=redpin
+	ldr r0,[r0]
+   	mov r1, #0
+   	bl digitalWrite
+   	b main
+
+
